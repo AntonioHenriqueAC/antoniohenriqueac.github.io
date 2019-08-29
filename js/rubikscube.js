aa60cee8715b689f78fc0ced9f6f79c9d38ad93f
Antonio Henrique <antoniohenriqueac@gmail.com>
2019-08-29 20:33:53 -0300

    exit rubiks game

--- js/rubikscube.js
+++ js/rubikscube.js
@@ -1,5 +1,3 @@
-// Three.js - https://github.com/mrdoob/three.js/
-// RoundedBoxGeometry - https://github.com/pailhead/three-rounded-box
 
 const animationEngine = ( () => {
 
@@ -944,14 +942,15 @@ const Easing = {
     InOut: () => t => ( 1 + Math.sin( Math.PI * t - Math.PI / 2 ) ) / 2,
 
   },
-
   Back: {
 
     Out: s => {
 
       s = s || 1.70158;
 
-      return t => { return ( t -= 1 ) * t * ( ( s + 1 ) * t + s ) + 1; };
+      return t => {
+        return (t -= 1) * t * ((s + 1) * t + s) + 1;
+      };
 
     },
 
@@ -959,11 +958,15 @@ const Easing = {
 
       s = s || 1.70158;
 
-      return t => { return t * t * ( ( s + 1 ) * t - s ); };
+      return t => {
+        return t * t * ((s + 1) * t - s);
+      };
 
     }
 
   },
+
+
 
   Elastic: {
 
@@ -3653,7 +3656,11 @@ const Icons = new IconsConverter( {
       viewbox: '0 0 512 512',
       content: '<path fill="currentColor" d="M444.788 291.1l42.616 24.599c4.867 2.809 7.126 8.618 5.459 13.985-11.07 35.642-29.97 67.842-54.689 94.586a12.016 12.016 0 0 1-14.832 2.254l-42.584-24.595a191.577 191.577 0 0 1-60.759 35.13v49.182a12.01 12.01 0 0 1-9.377 11.718c-34.956 7.85-72.499 8.256-109.219.007-5.49-1.233-9.403-6.096-9.403-11.723v-49.184a191.555 191.555 0 0 1-60.759-35.13l-42.584 24.595a12.016 12.016 0 0 1-14.832-2.254c-24.718-26.744-43.619-58.944-54.689-94.586-1.667-5.366.592-11.175 5.459-13.985L67.212 291.1a193.48 193.48 0 0 1 0-70.199l-42.616-24.599c-4.867-2.809-7.126-8.618-5.459-13.985 11.07-35.642 29.97-67.842 54.689-94.586a12.016 12.016 0 0 1 14.832-2.254l42.584 24.595a191.577 191.577 0 0 1 60.759-35.13V25.759a12.01 12.01 0 0 1 9.377-11.718c34.956-7.85 72.499-8.256 109.219-.007 5.49 1.233 9.403 6.096 9.403 11.723v49.184a191.555 191.555 0 0 1 60.759 35.13l42.584-24.595a12.016 12.016 0 0 1 14.832 2.254c24.718 26.744 43.619 58.944 54.689 94.586 1.667 5.366-.592 11.175-5.459 13.985L444.788 220.9a193.485 193.485 0 0 1 0 70.2zM336 256c0-44.112-35.888-80-80-80s-80 35.888-80 80 35.888 80 80 80 80-35.888 80-80z" />',
     },
-    back: {
+        back: {
+          viewbox: '0 0 512 512',
+          content: '<path transform="translate(512, 0) scale(-1,1)" fill="currentColor" d="M503.691 189.836L327.687 37.851C312.281 24.546 288 35.347 288 56.015v80.053C127.371 137.907 0 170.1 0 322.326c0 61.441 39.581 122.309 83.333 154.132 13.653 9.931 33.111-2.533 28.077-18.631C66.066 312.814 132.917 274.316 288 272.085V360c0 20.7 24.3 31.453 39.687 18.164l176.004-152c11.071-9.562 11.086-26.753 0-36.328z" />',
+        },
+    back2: {
       viewbox: '0 0 512 512',
       content: '<path transform="translate(512, 0) scale(-1,1)" fill="currentColor" d="M503.691 189.836L327.687 37.851C312.281 24.546 288 35.347 288 56.015v80.053C127.371 137.907 0 170.1 0 322.326c0 61.441 39.581 122.309 83.333 154.132 13.653 9.931 33.111-2.533 28.077-18.631C66.066 312.814 132.917 274.316 288 272.085V360c0 20.7 24.3 31.453 39.687 18.164l176.004-152c11.071-9.562 11.086-26.753 0-36.328z" />',
     },
@@ -3693,12 +3700,12 @@ const STATE = {
 };
 
 const BUTTONS = {
-  Menu: [ 'stats', 'prefs' ],
-  Playing: [ 'back' ],
+  Menu: [ 'stats', 'prefs', 'back2' ],
+  Playing: ['back', 'back2'],
   Complete: [],
   Stats: [],
-  Prefs: [ 'back', 'theme' ],
-  Theme: [ 'back', 'reset' ],
+  Prefs: ['back', 'theme', 'back2'],
+  Theme: ['back', 'reset', 'back2'],
   None: [],
 };
 
@@ -3713,6 +3720,7 @@ class Game {
       ui: document.querySelector( '.ui' ),
       game: document.querySelector( '.ui__game' ),
       back: document.querySelector( '.ui__background' ),
+      back2: document.querySelector( '.ui__background' ),
       prefs: document.querySelector( '.ui__prefs' ),
       theme: document.querySelector( '.ui__theme' ),
       stats: document.querySelector( '.ui__stats' ),
@@ -3727,6 +3735,7 @@ class Game {
       buttons: {
         prefs: document.querySelector( '.btn--prefs' ),
         back: document.querySelector( '.btn--back' ),
+        back2: document.querySelector('.btn--back2'),
         stats: document.querySelector( '.btn--stats' ),
         reset: document.querySelector( '.btn--reset' ),
         theme: document.querySelector( '.btn--theme' ),
@@ -3835,6 +3844,10 @@ class Game {
       }
 
     };
+this.dom.buttons.back2.onclick = event => {
+
+  window.location.replace("./index.html");
+};
 
     this.dom.buttons.reset.onclick = event => {
 
