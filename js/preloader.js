071d3516b8ed1649c569b05480f177fe8f5aff25
Antonio Cardoso <antonio.cardoso@gerencianet.com.br>
2020-03-27 14:35:13 -0300

    melhora preloader

--- js/preloader.js
+++ js/preloader.js
@@ -1,47 +1,47 @@
-var i = 0;
-var txt = '                                    Cardoso';
-var speed = 50;
+// var i = 0;
+// var txt = '                                    Cardoso';
+// var speed = 50;
 
-function typeWriterLeft() {
-  if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, 3500)
+// function typeWriterLeft() {
+//   if (i < txt.length) {
+//     document.getElementById("demo").innerHTML += txt.charAt(i);
+//     i++;
+//     setTimeout(typeWriter, 3500)
 
-  }
-}
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed)
+//   }
+// }
+// function typeWriter() {
+//   if (i < txt.length) {
+//     document.getElementById("demo").innerHTML += txt.charAt(i);
+//     i++;
+//     setTimeout(typeWriter, speed)
 
-  }
-}
+//   }
+// }
 
-$(document).ready(function () {
-  $(window).on('load', function () {
+// $(document).ready(function () {
+//   $(window).on('load', function () {
 
-    function Preloader() {
-      setTimeout(() => {
-        document.getElementById('preloader').style.display = 'none';
-        document.getElementById('home').style.display = 'block';
-      }, 3500);
-    }
+//     function Preloader() {
+//       setTimeout(() => {
+//         document.getElementById('preloader').style.display = 'none';
+//         document.getElementById('home').style.display = 'block';
+//       }, 3500);
+//     }
 
-    if (!sessionStorage.getItem('doNotShow')) {
-      sessionStorage.setItem('doNotShow', true);
-      Preloader();
-      typeWriterLeft()
+//     if (!sessionStorage.getItem('doNotShow')) {
+//       sessionStorage.setItem('doNotShow', true);
+//       Preloader();
+//       typeWriterLeft()
       
-    } else {
-      setTimeout(() => {
-        document.getElementById('preloader').style.display = 'none';
-        document.getElementById('home').style.display = 'block';
-      }, 1000);
-      typeWriter()
-    }
+//     } else {
+//       setTimeout(() => {
+//         document.getElementById('preloader').style.display = 'none';
+//         document.getElementById('home').style.display = 'block';
+//       }, 1000);
+//       typeWriter()
+//     }
 
-  });
+//   });
 
-});
+// });
