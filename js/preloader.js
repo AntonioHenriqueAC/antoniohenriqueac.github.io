ec3789e8f59bfd2bbf4ce703dd8c7fdf7f56e9af
Antonio Cardoso <antonio.cardoso@gerencianet.com.br>
2020-03-27 14:28:58 -0300

    add event to index page

--- js/preloader.js
+++ js/preloader.js
@@ -1,10 +1,47 @@
-var myVar;
+var i = 0;
+var txt = '                                    Cardoso';
+var speed = 50;
 
-function toLoading() {
-  myVar = setTimeout(showPage, 4000);
+function typeWriterLeft() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, 3500)
+
+  }
+}
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed)
+
+  }
 }
 
-function showPage() {
-  document.getElementById('preloader').style.display = 'none';
-  document.getElementById('home').style.display = 'block';
-}
+$(document).ready(function () {
+  $(window).on('load', function () {
+
+    function Preloader() {
+      setTimeout(() => {
+        document.getElementById('preloader').style.display = 'none';
+        document.getElementById('home').style.display = 'block';
+      }, 3500);
+    }
+
+    if (!sessionStorage.getItem('doNotShow')) {
+      sessionStorage.setItem('doNotShow', true);
+      Preloader();
+      typeWriterLeft()
+      
+    } else {
+      setTimeout(() => {
+        document.getElementById('preloader').style.display = 'none';
+        document.getElementById('home').style.display = 'block';
+      }, 1000);
+      typeWriter()
+    }
+
+  });
+
+});
